ffce4227098e732cd97a7f835cf7f68383075182
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-05-05 19:55:43 -0300

    first commit

---
+++ src/main/java/com/wise/buddy/wiseBuddy/WiseBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.wise.buddy.wiseBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wise/buddy/wiseBuddy/WiseBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.buddy.wiseBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
